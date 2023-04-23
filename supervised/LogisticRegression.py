abf56ae0456e777ed161567958f9ef44badb88f6
apatel1 <apatel1@loyalty.com>
2023-04-23 15:07:27 -0400

    Add new files and modify existing files to update project dependencies for Python 3.10.

--- supervised/LogisticRegression.py
+++ supervised/LogisticRegression.py
@@ -2,6 +2,8 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 from lab_utils_common import draw_vthresh
+from supervised.plt_one_addpt_onclick import plt_one_addpt_onclick
+
 plt.style.use('./deeplearning.mplstyle')
 
 # Input is an array.
@@ -39,3 +41,12 @@ ax.set_title("Sigmoid function")
 ax.set_ylabel('sigmoid(z)')
 ax.set_xlabel('z')
 draw_vthresh(ax,0)
+
+
+x_train = np.array([0., 1, 2, 3, 4, 5])
+y_train = np.array([0,  0, 0, 1, 1, 1])
+
+w_in = np.zeros((1))
+b_in = 0
+plt.close('all')
+addpt = plt_one_addpt_onclick( x_train,y_train, w_in, b_in, logistic=True)
